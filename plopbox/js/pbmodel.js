1526fe56adf13b879695a3b170b0e467c326ee8e
randfur <randfur@gmail.com>
2016-07-30 15:03:29 +1000

    Make model a simple object literal

--- plopbox/js/pbmodel.js
+++ plopbox/js/pbmodel.js
@@ -1,123 +1,84 @@
-// Model Object Constructor
-var ModelConstructor = (function () {
-  function modelObject () {
-    var self = this;
-    var view = null;
+'use strict';
 
-    // Recieve View Object Reference
-    this.viewRef = function (viewObject) {
-      if (!view) {
-        var view = viewObject
+var model = {
+  update: function (view, controller) {
+    // File Index
+    if (pagedata.opcode === 'FileIndex') {
+      if (pagedata.statcode == 'ViewDeny' || pagedata.statcode == 'Empty') {
+
       } else {
-        return;
-      }
-    }
+        // File Entry Object Constructor
+        function itemEntryObject (fileData, itemcount, ID) {
+          if (fileData['dir']) {
+            this.id = ID;
+            this.dir = filedata['dir'];
+            this.fileName = fileData['name'];
+            this.uri = fileData['path'];
+            this.icon = fileData['ficon'];
+            this.modTime = fileData['mtime'];
+            this.size = fileData['fsize'];
+            this.sizefactor = fileData['fsizefactor'];
+            this.modTimeUX = filedata['mtimeux'];
+          } else {
+            this.id = ID;
+            this.dir = filedata['dir'];
+            this.fileName = fileData['name'];
+            this.uri = fileData['path'];
+            this.icon = fileData['ficon'];
+            this.modTime = fileData['mtime'];
+            this.size = fileData['fsize'];
+            this.sizefactor = fileData['fsizefactor'];
+            this.modTimeUX = filedata['mtimeux'];
+          };
 
-    this.update = function () {
-      // File Index
-      if (pagedata.opcode === 'FileIndex') {
-        if (pagedata.statcode == 'ViewDeny' || pagedata.statcode == 'Empty') {
+          // File Operation Functions
+          this.open = function () {
 
-        } else {
-          // File Entry Object Constructor
-          function itemEntryObject (fileData, itemcount, ID) {
-            if (fileData['dir']) {
-              this.id = ID;
-              this.dir = filedata['dir'];
-              this.fileName = fileData['name'];
-              this.uri = fileData['path'];
-              this.icon = fileData['ficon'];
-              this.modTime = fileData['mtime'];
-              this.size = fileData['fsize'];
-              this.sizefactor = fileData['fsizefactor'];
-              this.modTimeUX = filedata['mtimeux'];
-            } else {
-              this.id = ID;
-              this.dir = filedata['dir'];
-              this.fileName = fileData['name'];
-              this.uri = fileData['path'];
-              this.icon = fileData['ficon'];
-              this.modTime = fileData['mtime'];
-              this.size = fileData['fsize'];
-              this.sizefactor = fileData['fsizefactor'];
-              this.modTimeUX = filedata['mtimeux'];
-            };
-
-            // File Operation Functions
-            this.open = function () {
-
-            }
-
-            this.move = function (dest) {
-
-            }
-
-            this.delete = function () {
-
-            }
           }
 
-          // Generate File Entries
-          for (i = 0; i < pagedata.itemcount; i++) {
-            pagedata.fileEntries[i] = new itemEntryObject(pagedata.filedata[i], pagedata.itemcount, i);
-          };
-        };
+          this.move = function (dest) {
 
-        // Navbar
-        var uriarray = nav.uri().split("/");
-        var lastnav = "";
-        for (i = 0, len = nav.uri().length; i < len; i++) {
-          pagedata.navItems[i] = lastnav + "/" + uriarray[i];
-          lastnav =+ uriarray[i];
-        };
+          }
 
-        // Stage Model Data
-        var modelData = {
-          opcode: pagedata.opcode,
-          statcode: pagedata.statcode,
-          msg: pagedata.msg,
-          crow: pagedata.crow,
-          filedata: pagedata.filedata,
-          loc: pagedata.loc,
-          sort: pagedata.sort
-        };
+          this.delete = function () {
 
-      } else if (pagedata.opcode == 'LoginPage') {
-        var modelData = {
-          opcode: pagedata.opcode,
-          statcode: pagedata.statcode,
-          token: pagedata.token,
-          msg: pagedata.msg,
+          }
         }
+
+        // Generate File Entries
+        for (i = 0; i < pagedata.itemcount; i++) {
+          pagedata.fileEntries[i] = new itemEntryObject(pagedata.filedata[i], pagedata.itemcount, i);
+        };
       };
-      view.update(modelData);
-    }
 
-    console.log("Model: I'm alive!");
-  }
+      // Navbar
+      var uriarray = nav.uri().split("/");
+      var lastnav = "";
+      for (i = 0, len = nav.uri().length; i < len; i++) {
+        pagedata.navItems[i] = lastnav + "/" + uriarray[i];
+        lastnav =+ uriarray[i];
+      };
 
+      // Stage Model Data
+      var modelData = {
+        opcode: pagedata.opcode,
+        statcode: pagedata.statcode,
+        msg: pagedata.msg,
+        crow: pagedata.crow,
+        filedata: pagedata.filedata,
+        loc: pagedata.loc,
+        sort: pagedata.sort
+      };
 
-  // Singleton Constructor Functions
-  var instance;
-  function createModel () {
-    var object = new modelObject();
-    return object;
-  }
-
-  return {
-    newModel: function () {
-      if (!instance) {
-        return createModel();
-      } else {
-        return;
+    } else if (pagedata.opcode == 'LoginPage') {
+      var modelData = {
+        opcode: pagedata.opcode,
+        statcode: pagedata.statcode,
+        token: pagedata.token,
+        msg: pagedata.msg,
       }
-    }
-  }
-})();
+    };
+    view.update(modelData, controller);
+  },
+};
 
-$(
-  function () {
-    // Create Model
-    var model = new ModelConstructor.newModel();
-  }
-);
